27a817d4be3a979e7894eb2d80d14ffb69dc2321
AdamSerag-cloud <adamserag25@gmail.com>
2021-04-17 15:04:29 -0700

    Redo

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -4,7 +4,7 @@ import { Button, Dropdown } from "semantic-ui-react";
 function Home() {
     return(
         <div>
-            <h1>Parking's a BEAC.</h1>
+            <h1>Parking's a BEACH.</h1>
             <h1>Let's find you a spot.</h1>
         </div>
     );
