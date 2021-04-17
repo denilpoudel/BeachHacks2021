235f5eb625503ad0719d70aa980d8093d0bfaeea
Denil <denilpoude@gmail.com>
2021-04-17 12:24:01 -0700

    Installed React Router and Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
