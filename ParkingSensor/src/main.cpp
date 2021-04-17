6bd159da32a62e131aad0b5fbdcb1a9efe688c00
Denil <denilpoude@gmail.com>
2021-04-17 13:51:10 -0700

    Ultrasonic Sensor determines distance

--- ParkingSensor/src/main.cpp
+++ ParkingSensor/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <NewPing.h>
+
+#define PING_PIN  5  // Arduino pin tied to both trigger and echo pins on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(PING_PIN, PING_PIN, MAX_DISTANCE); // NewPing setup of pin and maximum distance.
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Serial Connected");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+   delay(1000);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
 }
