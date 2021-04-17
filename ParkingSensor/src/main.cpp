ec4353c9fe84c2ff9bff05e86bf6bbf1417be89a
Denil <denilpoude@gmail.com>
2021-04-17 15:20:43 -0700

    Detects x times and outputs true if above y times

--- ParkingSensor/src/main.cpp
+++ ParkingSensor/src/main.cpp
@@ -4,11 +4,39 @@
 #define PING_PIN  5  // Arduino pin tied to both trigger and echo pins on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  5 
+#define TIME_TO_SLEEP  5 //Change to 300 for 5mins for non testing
+#define LOOP_TIMES 5    //The amount of times ulrasonic sensor files
+#define DETECTED_TIMES 3  //The amount of times 
+#define ID 12;
 
 RTC_DATA_ATTR int bootCount = 0;
 
+bool detected = false; 
+
 NewPing sonar(PING_PIN, PING_PIN, MAX_DISTANCE); // NewPing setup of pin and maximum distance.
+
+void ultraSonicSensor(){
+
+  int pulsedUltraSensor = 0;
+
+  for (int i = 0; i<LOOP_TIMES; i++){
+    delay(1000);           
+    int ping_cm = sonar.ping_cm();   
+    if (ping_cm >= 1)
+      pulsedUltraSensor += 1;
+      Serial.println(ping_cm);
+      Serial.println(pulsedUltraSensor);
+  }
+
+  if (pulsedUltraSensor >= DETECTED_TIMES) {
+    detected = true;
+    Serial.println("DETECTED");
+  }else
+    Serial.println("NOT DETECTED");
+
+
+}
+
 
 void print_wakeup_reason(){
   esp_sleep_wakeup_cause_t wakeup_reason;
@@ -17,11 +45,7 @@ void print_wakeup_reason(){
 
   switch(wakeup_reason)
   {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
     case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
     default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
   }
 }
@@ -36,13 +60,17 @@ void setup() {
   ++bootCount;
   Serial.println("Boot number: " + String(bootCount));
 
-  //Print the wakeup reason for ESP32
   print_wakeup_reason();
+  ultraSonicSensor();
+  
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+ 
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
 }
 
 void loop() {
-  delay(1000);                    // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
+  
 }
