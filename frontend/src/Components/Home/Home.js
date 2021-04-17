212ff15410558821e4ba586b55fb9f027e0b10d0
AdamSerag-cloud <adamserag25@gmail.com>
2021-04-17 15:00:49 -0700

    Started working on home page and included Montserrat font.

---
+++ frontend/src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button, Dropdown } from "semantic-ui-react";
+
+function Home() {
+    return(
+        <div>
+            <h1>Parking's a BEACH.</h1>
+            <h1>Let's find you a spot.</h1>
+        </div>
+    );
+}
---
+++ frontend/src/Constants/routes.js
@@ -0,0 +1,3 @@
+export const HOME ="/";
+export const ANYSPOT = "/anyspot";
+export const SPECIFICSPOT = "/specificspot";
